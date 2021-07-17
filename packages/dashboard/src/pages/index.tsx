68e8cb36e5361afa0a510529784a74a312f5cdee
jctaoo <jctaoo@outlook.com>
2021-07-18 00:54:29 +0800

    chore: setup dashboard project

---
+++ packages/dashboard/src/pages/index.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from '@/styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>TypeScript starter for Next.js</title>
+        <meta
+          name="description"
+          content="TypeScript starter for Next.js that includes all you need to build amazing apps"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+      <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">fuseement !</a>
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=typescript-nextjs-starter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{`fusee-code-lab`}
+        </a>
+      </footer>
+    </div>
+  );
+}
